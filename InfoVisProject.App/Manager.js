2cfd182ca182bf175aadb0135045af6f596a6d7a
Bernhard Rainer <bernhard.rainer@tuwien.ac.at>
2017-05-03 01:04:39 +0200

    implemented LodRendering

---
+++ InfoVisProject.App/Manager.js
@@ -0,0 +1,58 @@
+﻿document.write('<scr' + 'ipt type="text/javascript" src="./data/stations.js" ></scr' + 'ipt>');
+
+var split = function (arr, chunkSize) {
+    var groups = [], i;
+    for (i = 0; i < arr.length; i += chunkSize) {
+        groups.push(arr.slice(i, i + chunkSize));
+    }
+    return groups;
+}
+
+function State(year1, year2, value, zoom) {
+    this.year1  = year;
+    this.year2  = year2;
+    this.value  = value;
+    this.zoom   = zoom;
+}
+
+
+var markers = stations.map(station => {
+    var id = station["Id"];
+    var lat = parseFloat(station["Lat"]);
+    var long = parseFloat(station["Long"]);
+    var name = station["Name"];
+    var year1 = 1990;
+    var year2 = 2017;
+    var value1 = 22.5;
+    var value2 = 22.6;
+
+    return new Marker(id, name, lat, long, year1, value1, year2, value2);
+});
+
+
+// min lod : 6
+// max lod : 11
+function filterStations(zoom)
+{
+    var level = Math.min(zoom - 6, 11 - 6);
+    var level = Math.max(level, 0);
+
+    var groupSize = 0.0;
+
+    if (level == 0)         groupSize = 37;   
+    else if(level == 1)     groupSize = 19;   
+    else if (level == 2)    groupSize = 10;  
+    else if (level == 3)    groupSize = 5;    
+    else if (level == 4)    groupSize = 3;  
+    else if (level == 5)    groupSize = 1; 
+    else if (level == 6)    groupSize = 1;
+    
+    var groupedMarkers = split(markers, groupSize);  
+    var groupedMarkers2 = groupedMarkers.map(group => { return Marker.average(group); });
+    return groupedMarkers2;
+}
+
+function filterDataSet(state)
+{
+
+}
---
+++ InfoVisProject.App/Marker.js
@@ -0,0 +1,117 @@
+﻿
+
+class Marker
+{
+
+    constructor(id, name, lat, long, year1, value1, year2, value2)
+    {
+        this.id = id;
+        this.name = name;
+        this.lat = lat;
+        this.long = long;
+        this.year1 = year1;
+        this.year2 = year2;
+        this.value1 = value1;
+        this.value2 = value2;    
+    }
+    get getId()
+    {
+        return id;
+    }
+    get getYear1()
+    {
+        return this.year1;
+    }
+    get getYear2()
+    {
+        return this.year2;
+    }
+    get getValue1()
+    {
+        return this.value1;
+    }
+    get getValue2()
+    {
+        return this.value2;
+    }
+    get getTooltip()
+    {
+        let html =
+            '<p>' + this.name + '</p>' +
+            '<ul style="list-style-type:none"> '+
+            '<li>'+this.year1 + ": " + this.value1+'</li>'+
+            '<li>'+this.year2 + ": " + this.value2+'</li>'+            
+            '</ul>';
+        return html;
+    }
+
+
+    get getRotation()
+    {       
+        var diff        = this.value2 - this.value1;        
+        var rotation    = -diff * 100.0;     
+        return Math.min(90.0, Math.max(-90.0, rotation));      
+    }
+
+    get getLatitude()
+    {
+        return this.lat;
+    }
+
+    get getLongitude()
+    {
+        return this.long;
+    }
+
+    get getColor() {
+        var rotation = this.getRotation;
+        if (rotation < 0) {
+           
+            var i = (-rotation / 90.0);
+            var interpolate = d3.interpolateRgb("yellow", "red");
+            return interpolate(i);
+        }
+        else {
+            var i = Math.abs(rotation / 90.0);
+           
+            var interpolate = d3.interpolateRgb("yellow", "green");
+            return interpolate(i);
+        }
+    }
+
+
+    static average(markers)
+    {
+        
+        if (markers.length <= 0)
+            return null;
+
+        var value1 = 0.0;
+        var value2 = 0.0;
+        var lat     = 0.0;
+        var long    = 0.0;
+
+        var id      = "";
+        var name    = "";
+
+        markers.forEach((element) => {
+            value1  += element.value1;
+            value2  += element.value2;
+            lat     += element.lat;
+            long    += element.long;
+            id      += element.id + "/";
+            name    += element.name + "/";
+        });
+
+        id      = id.substring(0, id.length - 1);
+        name    = name.substring(0, name.length - 1);
+
+        lat     = lat    / markers.length;
+        long    = long   / markers.length;
+        value1  = value1 / markers.length;
+        value2  = value2 / markers.length;
+
+        return new Marker(id, name, lat, long, markers[0].year1, value1, markers[0].year2, value2);
+    }
+
+}
